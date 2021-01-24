1a869a88b2ad85722562aa52bb0e82676bf7abe1
wiro34 <too@wirog.net>
2021-01-24 22:08:20 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Card Simulator</h1>
+      aaa
+    </div>
+  );
+}
+
+export default App;
